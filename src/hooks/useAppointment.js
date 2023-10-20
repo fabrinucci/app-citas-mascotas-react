578e4bd54747500ef8f4399c7e5a4f00a1609dca
Fabrizio <fabrinucci99@gmail.com>
2023-10-20 19:14:04 -0300

    Change language project to Astro

--- src/hooks/useAppointment.js
+++ src/hooks/useAppointment.js
@@ -1,25 +1,22 @@
-import { useState } from "react";
+import { useState } from 'react';
 
+console.log('hellor');
 
 export const useAppointment = () => {
-
-  let initValue = JSON.parse(localStorage.getItem('appointments'))
-  if( !initValue ) {
-    initValue = []
+  let initValue = JSON.parse(localStorage.getItem('appointments'));
+  if (!initValue) {
+    initValue = [];
   }
 
   const [appointments, saveAppointments] = useState(initValue);
 
-  const createAppointment = appointment => {
-  saveAppointments([
-    ...appointments,
-    appointment
-    ]
-  )}
+  const createAppointment = (appointment) => {
+    saveAppointments([...appointments, appointment]);
+  };
 
   return {
     appointments,
-    saveAppointments, 
-    createAppointment
-  }
-}
+    saveAppointments,
+    createAppointment,
+  };
+};
